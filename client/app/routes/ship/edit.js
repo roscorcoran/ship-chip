42289f88be93088cb260744912a6feceaa78281b
Ros Corcoran <roscorcoran@gmail.com>
2016-08-08 17:22:12 +0100

    Styling and console cleanup

--- client/app/routes/ship/edit.js
+++ client/app/routes/ship/edit.js
@@ -2,12 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-  //model: function() {
-  //  return this.store.find('ship', );
-  //}
-
   model: function (params) {
-    console.log(params, this.store.findRecord('ship', params.id));
     return this.store.findRecord('ship', params.id);
   }
 
