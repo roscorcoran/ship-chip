e32a96cd7fa6bcd4921f45776ef54a23830c482d
Ros Corcoran <roscorcoran@gmail.com>
2016-08-09 20:22:57 +0100

    Refactor the config module and improve code structure - clean code

--- index.js
+++ index.js
@@ -20,20 +20,25 @@ var configFile = './run/config.json';
 var db = firebase.database();
 var ships = db.ref("ships");
 
+//Use the config module to get a current or generate a new config.
 var config = new Config(configFile, ships);
 
 //Once we have our configuration loaded from the server we can run all defined IO event handlers
 config.once('loaded', (config) => {
+  //Get a reference to this ships model
   var shipRef = ships.child(config.key);
-  var IORef = shipRef.child('io');
 
-  IORef.on("child_changed", function (snapshot) {
-    var changedIO = snapshot.val();
-    console.log("Changed IO", changedIO);
-  });
-
+  //An event handler for this specific Ship
   shipRef.on("child_changed", function (snapshot) {
-    var changedShip = snapshot.val();
-    console.log("Are you talking to me?", changedShip);
+    var newValue = snapshot.val();
+    console.log(snapshot.key + ' changed to: ', newValue);
   });
+
+  //Event handler for this Ships soft-io changes.
+  var IORef = shipRef.child('softIo');
+  IORef.on("child_changed", function (snapshot) {
+    var newValue = snapshot.val();
+    console.log("Changed IO", newValue);
+  });
+
 });
--- shipConfig.js
+++ shipConfig.js
@@ -1,53 +1,94 @@
 var util = require('util');
 var fs = require('fs');
+var os = require('os');
 var EventEmitter = require('events').EventEmitter;
 
 // @station - an object with `freq` and `name` properties
-var Config = function(configFile, ships) {
+var Config = function (configFile, ships) {
 
   // we need to store the reference of `this` to `self`, so that we can use the current context in the setTimeout (or any callback) functions
   // using `this` in the setTimeout functions will refer to those functions, not the Radio class
   var self = this;
 
   //Check for a local config ref, if we have one load it from the server, if not make a new one
-  fs.stat(configFile, (err, stat) => {
-    if (!err && stat.isFile()) {
-      loadConfig();
+  configExists(function (exists) {
+    if (exists) {
+      getShipId(function (id) {
+        loadConfigFromServer(id, function (config) {
+          self.emit('loaded', config);
+        });
+        //Update the network model for this device (firebase push to server)
+        updateNetworkStatus(id);
+      });
     } else {
-      autoConfig();
+      autoConfig(function(config){
+        self.emit('loaded', config);
+      });
     }
   });
 
-
-  function loadConfig() {
-    fs.readFile(configFile, (err, data) => {
-      if (err) throw err;
-      var config = JSON.parse(data.toString());
-      var shipRef = ships.child(config.key);
-      shipRef.once("value", (snapshot) => {
-        var newConfig = snapshot.val();
-        newConfig.key = config.key;
-        saveConfig(newConfig);
-      });
+  function loadConfigFromServer(id, cb) {
+    var shipRef = ships.child(id);
+    shipRef.once("value", (snapshot) => {
+      var newConfig = snapshot.val();
+      newConfig.key = id;
+      cb(newConfig);
     });
   }
 
-  function autoConfig() {
+  function autoConfig(cb) {
     //Auto register, so make a new config and save it locally
     var newShipRef = ships.push();
     var config = {
       name: "A NEW SHIP",
-      description: "Configure me!"
+      description: "Configure me!",
+      net: getNetworkInterfacesIPV4()
     };
     newShipRef.set(config);
     config.key = newShipRef.key;
-    saveConfig(config);
+    //Save the key only
+    saveConfig(config.key);
+    cb(config);
   }
 
-  function saveConfig(config) {
-    self.emit('loaded', config);
-    fs.writeFile(configFile, JSON.stringify(config, null, 2), (err) => {
+  function saveConfig(id) {
+    fs.writeFile(configFile, JSON.stringify({id: id}, null, 2), (err) => {
       if (err) throw err;
+    });
+  }
+
+  function updateNetworkStatus(id){
+    var shipRef = ships.child(id);
+    var netRef = shipRef.child('net');
+    netRef.set(getNetworkInterfacesIPV4());
+  }
+
+  function getNetworkInterfacesIPV4() {
+    var interfaces = os.networkInterfaces();
+    //Get the IP addresses
+    var addresses = [];
+    for (var k in interfaces) {
+      for (var k2 in interfaces[k]) {
+        var address = interfaces[k][k2];
+        if (address.family === 'IPv4' && !address.internal) {
+          addresses.push(address);
+        }
+      }
+    }
+    return addresses;
+  }
+
+  function getShipId(cb) {
+    fs.readFile(configFile, (err, data) => {
+      if (err) throw err;
+      var config = JSON.parse(data.toString());
+      cb(config.id);
+    });
+  }
+
+  function configExists(cb) {
+    fs.stat(configFile, (err, stat) => {
+      cb(!err && stat.isFile());
     });
   }
 
