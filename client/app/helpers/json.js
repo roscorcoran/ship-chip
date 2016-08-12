460be592c002558fe8bdd7d0ef662c86d45bfb19
Ros Corcoran <roscorcoran@gmail.com>
2016-08-12 12:14:50 +0100

    Heed linting warnings

--- client/app/helpers/json.js
+++ client/app/helpers/json.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Helper.extend({
-  compute(params, hash) {
+  //compute(params, hash) {
+  compute(params) {
     return JSON.stringify(params[0], null, 2);
   }
 });
