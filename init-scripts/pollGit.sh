4f7ce5fccfa34d6d7a4e341b8a0fde60bef3ea84
Ros Corcoran <roscorcoran@gmail.com>
2016-08-17 21:45:53 +0100

    Update init-scripts to use systemctl service

--- init-scripts/pollGit.sh
+++ init-scripts/pollGit.sh
@@ -2,16 +2,16 @@
 #move into your git repo
 cd /opt/ship-chip;
 
-git fetch;
-LOCAL=$(git rev-parse HEAD);
-REMOTE=$(git rev-parse @{u});
+/usr/bin/git fetch;
+LOCAL=$(/usr/bin/git rev-parse HEAD);
+REMOTE=$(/usr/bin/git rev-parse @{u});
 
 #if our local revision id doesn't match the remote, we will need to pull the changes
 if [ $LOCAL != $REMOTE ]; then
     #stop the service before making changes
-    service chip-ship stop;
+    systemctl stop shipchip.service;
     #pull and merge changes
     git pull origin master;
     #start the service reflecting new changes
-    service chip-ship start;
+    systemctl start shipchip.service;
 fi
