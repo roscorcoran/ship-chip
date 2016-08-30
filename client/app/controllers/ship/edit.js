c6f585ee7fd6ab77c9370cb668e5152d4593cab8
Ros Corcoran <roscorcoran@gmail.com>
2016-08-30 17:24:07 +0100

    Add 'softIO' button toggles && nicer button styles

--- client/app/controllers/ship/edit.js
+++ client/app/controllers/ship/edit.js
@@ -3,7 +3,11 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 
   actions: {
-    updateShip: function(ship) {
+    updateShip: function (ship) {
+      ship.save();
+    },
+    toggleButton: function (ship, b) {
+      Ember.set(b, 'value', !Ember.get(b, 'value'));
       ship.save();
     }
   }
